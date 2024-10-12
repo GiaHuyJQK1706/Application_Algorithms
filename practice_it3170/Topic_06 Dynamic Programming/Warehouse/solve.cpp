014fb0e666985c64006d1314095cdd6f7d70d50e
Gia-Huy Do Hust <huydogiaccac@gmail.com>
2024-10-13 01:48:20 +0700

    Update solve.cpp

--- practice_it3170/Topic_06 Dynamic Programming/Warehouse/solve.cpp
+++ practice_it3170/Topic_06 Dynamic Programming/Warehouse/solve.cpp
@@ -13,30 +13,30 @@ int res = 0;
 int main() {
     cin >> N >> T >> D;
     vector<int> a(N), t(N);
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         cin >> a[i];
     }
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         cin >> t[i];
     }
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         if (t[i] <= T) {
             dp[i][t[i]] = a[i];
         }
     }
 
-    for (int i = 0; i < N; ++i) {
-        for (int time = 0; time <= T; ++time) {
+    for (int i = 0; i < N; i++) {
+        for (int time = 0; time <= T;time++) {
             if (dp[i][time] == 0) continue;
-            for (int j = i + 1; j <= i + D && j < N; ++j) {
+            for (int j = i + 1; j <= i + D && j < N; j++) {
                 if (time + t[j] <= T) {
                     dp[j][time + t[j]] = max(dp[j][time + t[j]], dp[i][time] + a[j]);
                 }
             }
         }
     }
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         for (int time = 0; time <= T; ++time) {
             res = max(res, dp[i][time]);
         }
