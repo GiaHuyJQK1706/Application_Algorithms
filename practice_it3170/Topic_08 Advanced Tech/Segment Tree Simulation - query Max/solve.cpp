b97ae3b98758bddb31fc5e78da6e566b631a6f89
Gia-Huy Do Hust <huydogiaccac@gmail.com>
2024-10-13 01:51:15 +0700

    Update solve.cpp

--- practice_it3170/Topic_08 Advanced Tech/Segment Tree Simulation - query Max/solve.cpp
+++ practice_it3170/Topic_08 Advanced Tech/Segment Tree Simulation - query Max/solve.cpp
@@ -72,7 +72,7 @@ int main() {
     int n;
     cin >> n;
     vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
@@ -80,7 +80,7 @@ int main() {
 
     int m;
     cin >> m;
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; i++) {
         string action;
         cin >> action;
         if (action == "get-max") {
